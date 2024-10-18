51f05c6d68a764b43c2e47257b16069a1763aefa
Talha Ahmed <t01haa2005@gmail.com>
2024-10-17 19:04:45 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -57,9 +57,9 @@ public:
         //here we are inserting newNode after the node at the given postion
         newNode->next = temp->next; // set the next of newNode to the next of temp
         newNode->prev = temp; //set the prev of newNode to temp (the current position)
-        if (temp->next)                 //if there is a node after temp, update its prev pointer
+        if (temp->next) //if there is a node after temp, update its prev pointer
             temp->next->prev = newNode;
-        else                            //if temp is the tail, update tail to newNode
+        else   //if temp is the tail, update tail to newNode
             tail = newNode;
         temp->next = newNode; //then finally you set temps next to newNode
     }
@@ -110,79 +110,82 @@ public:
             else
                 temp = temp->next;
         }
-        //if
+        //if position doesent exist, return message
         if (!temp) {
             cout << "Position doesn't exist." << endl;
             return;
         }
-    
+        //if deleting the last node, pop back
         if (!temp->next) {
             pop_back();
             return;
         }
-    
+        //adjust the previous and next pointers of surrounding nodes
         Node* tempPrev = temp->prev;
         tempPrev->next = temp->next;
         temp->next->prev = tempPrev;
+        //delete node
         delete temp;
     }
-
+    //a method to add a node at end of the list
     void push_back(int v) {
-        Node* newNode = new Node(v);
-        if (!tail)
+        Node* newNode = new Node(v); //create new node
+        if (!tail)  //if list is empty, set both heads and tail to newNode
             head = tail = newNode;
-        else {
+        else {  //otherwise, link the new node to the end and update tail
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
     }
-    
+    //adding node at front of list 
     void push_front(int v) {
-        Node* newNode = new Node(v);
+        Node* newNode = new Node(v); //create new node
         if (!head)
             head = tail = newNode;
-        else {
+        else { //otherwise link the new node to the front and update head
             newNode->next = head;
             head->prev = newNode;
             head = newNode;
         }
     }
-    
+    //method to remove the first node from the list
     void pop_front() {
 
-        if (!head) {
+        if (!head) { //if the list is empty print message and return
             cout << "List is empty." << endl;
             return;
         }
 
-        Node * temp = head;
+        Node * temp = head; //this is a temporary pointer to store the current head
 
+        //update head to next node in list
         if (head->next) {
-            head = head->next;
-            head->prev = nullptr;
+            head = head->next; 
+            head->prev = nullptr; //update prev of the new head
         }
         else
-            head = tail = nullptr;
-        delete temp;
+            head = tail = nullptr; //if this was the only node, set head and tail to nulptr
+        delete temp; //delete old head
     }
-
+    //way to remove last node from list
     void pop_back() {
-        if (!tail) {
+        if (!tail) { //if the list is empty, print message and return
             cout << "List is empty." << endl;
             return;
         }
-        Node * temp = tail;
+        Node * temp = tail; //temporary pointer to store current tail
 
+        //update tail to previous node
         if (tail->prev) {
             tail = tail->prev;
-            tail->next = nullptr;
+            tail->next = nullptr; //update next of the new tail
         }
         else
-            head = tail = nullptr;
-        delete temp;
+            head = tail = nullptr; //if this was the only node, set head and tail to nullptr
+        delete temp; //delete old tail
     }
-
+    //
     ~DoublyLinkedList() {
         while (head) {
             Node* temp = head;
